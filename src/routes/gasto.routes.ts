54bd8a18e57aa0f0c7887e8a3e2f28fdd4514a72
Jefferson Sarango 1994 <jeffersonk132008@hotmail.com>
2025-08-12 20:01:06 -0500

    Desplegando Base

--- src/routes/gasto.routes.ts
+++ src/routes/gasto.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  GastosController } from "../controllers/GastoController";
+import {  GastoController } from "../controllers/GastoController";
 
 const router = Router();
 
