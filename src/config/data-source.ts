1e3d56df6d4c09e634c8025c2403913fda77daf6
Jefferson Sarango 1994 <jeffersonk132008@hotmail.com>
2025-09-22 21:18:36 -0500

    cambio en imagen empresa y cambio en producto precio de vennta

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -26,7 +26,7 @@ import {Reserva } from '../entities/Reserva'
 import {EstadoReserva } from '../entities/EstadoReserva'
 
 //cadeana local
-export const AppDataSource = new DataSource({
+/*export const AppDataSource = new DataSource({
   type: 'mssql',
   host: 'localhost', // o LAPTOP-UA0JFMK2
   port: 1433,
@@ -44,10 +44,10 @@ export const AppDataSource = new DataSource({
     encrypt: false,
     enableArithAbort: true,
   },
-});
+});*/
 
 
-/*export const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'mssql',
   host: 'SQL1004.site4now.net', // Actualizado según "Data Source"
   port: 1433, // Puerto predeterminado para SQL Server, generalmente 1433
@@ -75,4 +75,4 @@ export const AppDataSource = new DataSource({
     trustServerCertificate: true, // Cambiar a false en producción y proporcionar certificados CA válidos
     enableArithAbort: true, // Habilita la validación de errores aritméticos
   },
-});*/
+});
